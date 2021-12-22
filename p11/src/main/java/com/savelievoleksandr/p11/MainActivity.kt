223d0f3e9566de91dbce28ab238979ac57416608
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2021-12-23 00:02:22 +0200

    added features to p11

--- p11/src/main/java/com/savelievoleksandr/p11/MainActivity.kt
+++ p11/src/main/java/com/savelievoleksandr/p11/MainActivity.kt
@@ -47,47 +47,5 @@ class MainActivity : AppCompatActivity() {
 
         }
         weatherViewModel.getWeather()
-
-//        weatherApiService.getToday(32.0617f, 49.4420f).enqueue(object : Callback<WeatherDay> {
-//            @SuppressLint("UseCompatLoadingForDrawables")
-//            override fun onResponse(call: Call<WeatherDay>, response: Response<WeatherDay>) {
-//                cityTextView.text = response.body()?.data?.get(0)?.city_name
-//                nowTempTextView.text = response.body()?.data?.get(0)?.temp.toString() + "℃"
-//                DateTextView.text = response.body()?.data?.get(0)?.ob_time?.substring(0, 11)
-//                nowWindTextView.text =
-//                    String.format("%.2f", response.body()?.data?.get(0)?.wind_spd)
-//                nowRHTextView.text = String.format("%.2f", response.body()?.data?.get(0)?.rh) + "%"
-//                nowImageView.setImageDrawable(
-//                    getDrawable(
-//                        resources.getIdentifier(
-//                            "@drawable/" + response.body()?.data?.get(0)?.weather?.icon,
-//                            null, packageName,
-//                        ),
-//                    ),
-//                )
-//            }
-//
-////            override fun onFailure(call: Call<WeatherDay>, t: Throwable) {
-////                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
-////            }
-//        })
-
-
-//        val hourlyWeatherApiService = retrofit.create(HourlyWeatherApiService::class.java)
-//        hourlyWeatherApiService.getToday(49.4420f, 32.0617f, 24)
-//            .enqueue(object : Callback<WeatherHourly> {
-//                override fun onResponse(
-//                    call: Call<WeatherHourly>,
-//                    response: Response<WeatherHourly>
-//                ) {
-//                    val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-//                    recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-//                    recyclerView.adapter = WeatherAdapter(response.body()!!)
-//                }
-//
-//                override fun onFailure(call: Call<WeatherHourly>, t: Throwable) {
-//                    Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
-//                }
-//            })
     }
 }
